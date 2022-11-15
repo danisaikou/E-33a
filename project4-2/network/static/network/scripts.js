3376df604428624d31b0940451dce9011ffdaad4
danisaikou <danisaikou@gmail.com>
2022-11-15 11:37:54 -0800

    working on js

--- project4-2/network/static/network/scripts.js
+++ project4-2/network/static/network/scripts.js
@@ -1,13 +1,37 @@
-function like(post_id) {
+function like(id) {
     const like_btn = document.getElementById("liked").style.visibility = 'hidden';
     const not_liked_btn = document.getElementById("not_liked").style.visibility = 'visible';
 
-    hidden.addEventListener('click', function onClick() {
+    like_btn.addEventListener('click', function onClick() {
        // update frm hidden to visible
+       if (like_btn.style.visibility === 'hidden') {
+            fetch(`/like/` + id, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    like: true
+                })
+            })
+            
+            like_btn.style.visibility = 'visible';
+
+            fetch(`/like/` + id, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    like: false
+                })
+            })
+            
+            like_btn.style.visibility = 'hidden';
+
+            fetch(`/like/` + `${id}`) 
+                .then(response => response.json())
+                .then(post => {
+                    like_count.innerHTML = post.likes;
+                });
+            }
+            return false;
       });
 }
-
-const btn = document.getElementById('btn');
 
 
 
