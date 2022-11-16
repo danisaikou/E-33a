14836586ab47a64811f92bd814e551888f0e606b
danisaikou <danisaikou@gmail.com>
2022-11-15 20:53:58 -0800

    hearts working

--- project4-2/network/static/network/scripts.js
+++ project4-2/network/static/network/scripts.js
@@ -1,37 +1,5 @@
-function like(id) {
-    const like_btn = document.getElementById("liked").style.visibility = 'hidden';
-    const not_liked_btn = document.getElementById("not_liked").style.visibility = 'visible';
+function like_func(x) {
+    x.classList.toggle("fa-heart").css('color','#df4759');
 
-    like_btn.addEventListener('click', function onClick() {
-       // update frm hidden to visible
-       if (like_btn.style.visibility === 'hidden') {
-            fetch(`/like/` + id, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    like: true
-                })
-            })
-            
-            like_btn.style.visibility = 'visible';
-
-            fetch(`/like/` + id, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    like: false
-                })
-            })
-            
-            like_btn.style.visibility = 'hidden';
-
-            fetch(`/like/` + `${id}`) 
-                .then(response => response.json())
-                .then(post => {
-                    like_count.innerHTML = post.likes;
-                });
-            }
-            return false;
-      });
+    
 }
-
-
-
