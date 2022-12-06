dc886bd76c7d780b8dcda84a911a7945c008a784
danisaikou <danisaikou@gmail.com>
2022-12-06 16:09:01 -0500

    navbar working

--- cs-darmok/darmoktrack/darmoktrack/urls.py
+++ cs-darmok/darmoktrack/darmoktrack/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apps.track import views 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
