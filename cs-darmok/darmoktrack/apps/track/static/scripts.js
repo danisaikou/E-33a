788a91522cc2f1e4f7d602cb4fd015cb9acb261e
danisaikou <danisaikou@gmail.com>
2022-12-08 17:01:53 -0500

    moved script back to scripts.js

--- cs-darmok/darmoktrack/apps/track/static/scripts.js
+++ cs-darmok/darmoktrack/apps/track/static/scripts.js
@@ -1,42 +1,46 @@
-// // Initialize the time clock with a starting time of 0
-// let time = 0;
+// Initialize time clock
+let time = 0;
+// Initilaie timer 
+let timer = null;
 
-// // Initialize the timer variable to null
-// let timer = null;
+//Start timer
+function startTimer() {
+    // if not running, start
+    if (!timer) {
+        timer = setInterval(function() {
+            time++;
+            //update display with new time
+            updateTimeDisplay();
+        }, 1000);
+    }
+}
 
-// // Function to start the timer
-// function startTimer() {
-//   // If the timer is not running, start it
-//   if (!timer) {
-//     timer = setInterval(function() {
-//       time++;
-//       // Update the time display with the new time
-//       updateTimeDisplay();
-//     }, 1000);
-//   }
-// }
+function stopTimer() {
+    //if running, stop
+    if (timer) {
+        clearInterval(timer);
+        timer=null;
+    }
+}
 
-// // Function to stop the timer
-// function stopTimer() {
-//   // If the timer is running, stop it
-//   if (timer) {
-//     clearInterval(timer);
-//     timer = null;
-//   }
-// }
+//update display
+function updateTimeDisplay() {
+    //get current time in h/m/s
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60 );
+    let seconds = time % 60;
 
-// // Function to update the time display
-// function updateTimeDisplay() {
-//   // Get the current time in hours, minutes, and seconds
-//   let hours = Math.floor(time / 3600);
-//   let minutes = Math.floor((time % 3600) / 60);
-//   let seconds = time % 60;
+    //format time as stringformat
+    let timeString = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-//   // Format the time as a string (e.g. "01:23:45")
-//   let timeString = `${hours.toString().padStart(2, "0")}:${minutes
-//     .toString()
-//     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    // update time display with fomratted string
+    document.getElementById("time-display").innerHTML = timeString;
+}
 
-//   // Update the time display with the formatted time string
-//   document.getElementById("time-display").innerHTML = timeString;
-// }
+function stopTimerAndSaveTime() {
+    // stop the timer
+    stopTimer();
+
+    // send time to server 
+    fetch
+}
